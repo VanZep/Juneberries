81152640cb0d6686b2e3737f6ce9f75a1224dcd6
VanZep <vanzup@mail.ru>
2025-07-28 01:16:36 +0300

    small fixes

--- auth_service/app/models/base.py
+++ auth_service/app/models/base.py
@@ -1,10 +1,12 @@
 from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
 
+from config import settings
+
 
 class Base(DeclarativeBase):
     """Базовая абстрактная модель."""
 
     __abstract__ = True
 
-    metadata = MetaData()
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
--- auth_service/app/routes/users.py
+++ auth_service/app/routes/users.py
@@ -6,20 +6,24 @@ from crud.users import create_user
 
 router = APIRouter(prefix=settings.api.auth, tags=['Users'])
 
+
 @router.post('/register')
 def register(user: CreateUser):
     """Регистрация пользователя."""
     return create_user(user)
+
 
 @router.post('/login')
 def login():
     """Логин пользователя."""
     pass
 
+
 @router.get('/me')
 def me():
     """Страница пользователя."""
     pass
+
 
 @router.post('/refresh')
 def refresh():
