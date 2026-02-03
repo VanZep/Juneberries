33077ddd5727c1e56fa6f5cd49bd76ee6d0b2cb3
VanZep <vanzup@mail.ru>
2026-02-03 11:28:27 +0300

    add UpdateUser schema in auth_service\app\schemas

--- auth_service/app/schemas/users.py
+++ auth_service/app/schemas/users.py
@@ -6,9 +6,13 @@ from pydantic import BaseModel, EmailStr
 class CreateUser(BaseModel):
     """Схема создания пользователя."""
 
-    id: int
     email: EmailStr
     password_hash: str
     name: str
     role_id: int
     created_at: datetime
+
+
+class UpdateUser(BaseModel):
+    """Схема обновления пользователя."""
+    pass
